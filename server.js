b1c3823a64bbfb26a65bc622f957345eaaca2a7b
Enock Omondi <enockomondi305@gmail.com>
2022-06-16 17:37:24 +0300

    connecting the application to the database

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// simple route for testing
+app.get('/', (req, res) =>{
+  res.send('Application running successfully')
+})
+
+// use dotenv
+require('dotenv').config();
+
+// connect to the database
+const db_url = process.env.DATABASE_URL;
+mongoose.connect(db_url, {useNewUrlParser: true})
+        .then(() =>{
+          console.log('Connected to Database successfully!');
+        })
+        .catch((error) =>{
+          console.log(error);
+        })
+        
+// USE MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// initialize a port
+const PORT = process.env.PORT || 9000;
+
+// listen to the port
+app.listen(PORT, () =>{
+  console.log(`Application running on port ${PORT}`);
+})
